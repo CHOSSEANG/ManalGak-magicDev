e535cace427e0f0374d78ef5c3832b5b71bbd9b2
김규리 <cherrychel@naver.com>
2026-01-13 12:22:40 +0900

    feat: 초대된 모임원 약속 요약 페이지 및 로그인

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -1,22 +1,20 @@
 // src/components/layout/Footer.tsx
 
-'use client'
+"use client";
 
 export default function Footer() {
   const handleFamilyLinkChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const url = e.target.value
-    if (!url) return
-    window.open(url, '_blank')
-    e.target.selectedIndex = 0 // 다시 placeholder로
-  }
+    const url = e.target.value;
+    if (!url) return;
+    window.open(url, "_blank");
+    e.target.selectedIndex = 0; // 다시 placeholder로
+  };
 
   return (
     <footer className="w-full border-t border-[var(--wf-border)]">
       <div className="mx-auto flex max-w-[1440px] items-center justify-between px-4 py-3 text-xs">
         {/* 왼쪽: 최소 브랜드 */}
-        <span className="text-[var(--wf-text-subtle)]">
-          © Moilgak
-        </span>
+        <span className="text-[var(--wf-text-subtle)]">© Manalgak</span>
 
         {/* 오른쪽: 패밀리 링크 */}
         <select
@@ -28,23 +26,23 @@ export default function Footer() {
             패밀리 링크
           </option>
           <option value="https://github.com/CHOSSEANG/ManalGak-magicDev.git">
-            모일각 GitHub
+            만날각 GitHub
           </option>
           <option value="https://www.notion.so/MagicDev-2d907cb40308808a871df339cb7b264c">
-            모일각 Notion
+            만날각 Notion
           </option>
           <option value="https://www.figma.com/design/bLBe9NwstkTnikd5h2tAJg/magicDev-%EC%B9%B4%EC%B9%B4%EC%98%A4api?node-id=0-1&t=S3iyx2Gzy9eHAAOR-1">
-            모일각 Figma 디자인회의
+            만날각 Figma 디자인회의
           </option>
           <option value="https://miro.com/welcomeonboard/WmNTMlNTUjc3ZHhNTDZOdUlmU1BSUllKU0NxY0tSTUNxT3pDdEFmdWRaWTlydURyUVVtMjk1L3pnL0swQnkrREMrcTNMdzhMei9hREpDUGtOTEFPSTl6Q0FLRkJacW9JT3UzZmlDN1J1NDZvV2k3Q3lKMEh2dnJ5K3BYMDhMQmJnbHpza3F6REdEcmNpNEFOMmJXWXBBPT0hdjE=?share_link_id=151136698">
-            모일각 miro 기획회의
+            만날각 miro 기획회의
           </option>
-          <option value="https://webicapp.com/">
-            이전 프로젝트 보기
-          </option>
+          <option value="https://webicapp.com/">이전 프로젝트 보기</option>
         </select>
       </div>
-      <br /><br /><br />
+      <br />
+      <br />
+      <br />
     </footer>
-  )
+  );
 }
