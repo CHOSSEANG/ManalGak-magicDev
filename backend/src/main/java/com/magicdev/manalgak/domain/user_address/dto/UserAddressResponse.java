8e5dd2b57ef1591b028f6be591902489605c23bd
napum <gkdmsl9933@naver.com>
2026-01-14 20:21:51 +0900

    주소 entity, dto

---
+++ backend/src/main/java/com/magicdev/manalgak/domain/user_address/dto/UserAddressResponse.java
@@ -0,0 +1,27 @@
+package com.magicdev.manalgak.domain.user_address.dto;
+
+import com.magicdev.manalgak.domain.user_address.entity.UserAddress;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Builder
+@Getter
+public class UserAddressResponse {
+
+    private Long id;
+    private Long userId;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+    private String address;
+
+    public static UserAddressResponse from(UserAddress userAddress){
+        return UserAddressResponse.builder()
+                .address(userAddress.getAddress())
+                .latitude(userAddress.getLatitude())
+                .userId(userAddress.getUser().getId())
+                .id(userAddress.getId())
+                .build();
+    }
+}
---
+++ backend/src/main/java/com/magicdev/manalgak/domain/user_address/entity/UserAddress.java
@@ -0,0 +1,48 @@
+package com.magicdev.manalgak.domain.user_address.entity;
+
+import com.magicdev.manalgak.domain.user.dto.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "user_address")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserAddress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(precision = 10, scale = 8)
+    private BigDecimal latitude;
+
+    @Column(precision = 11,scale = 8)
+    private BigDecimal longitude;
+
+    @Column(length = 255)
+    private String address;
+
+    public UserAddress(User user, String address, BigDecimal latitude, BigDecimal longitude) {
+        this.user = user;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public void update(String address, BigDecimal latitude, BigDecimal longitude) {
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+
+}
