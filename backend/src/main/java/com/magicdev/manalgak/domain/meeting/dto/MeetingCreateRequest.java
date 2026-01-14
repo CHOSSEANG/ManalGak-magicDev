150cd5107f61884c582cabc8097d1dfee42c4980
napum <gkdmsl9933@naver.com>
2026-01-14 12:26:02 +0900

    모임 DTO

--- backend/src/main/java/com/magicdev/manalgak/domain/meeting/dto/MeetingCreateRequest.java
+++ backend/src/main/java/com/magicdev/manalgak/domain/meeting/dto/MeetingCreateRequest.java
@@ -3,16 +3,21 @@ package com.magicdev.manalgak.domain.meeting.dto;
 import com.magicdev.manalgak.domain.meeting.entity.Meeting;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 
 @Getter
 public class MeetingCreateRequest {
 
     private String meetingName;
+    private String meetingNickName;
+    private LocalDateTime meetingTime;
 
     public Meeting toEntity(Long organizerId){
         Meeting meeting = new Meeting();
         meeting.setMeetingName(this.meetingName);
         meeting.setOrganizerId(organizerId);
+        meeting.setMeetingTime(this.meetingTime);
         return meeting;
     }
 }
