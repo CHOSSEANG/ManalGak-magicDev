9e3c5036003e2647db9592e04c12490ac5a59c97
napum <gkdmsl9933@naver.com>
2026-01-15 13:51:26 +0900

    모임전체조회 페이징처리

--- backend/src/main/java/com/magicdev/manalgak/domain/meeting/repository/MeetingRepository.java
+++ backend/src/main/java/com/magicdev/manalgak/domain/meeting/repository/MeetingRepository.java
@@ -1,12 +1,17 @@
 package com.magicdev.manalgak.domain.meeting.repository;
 
 import com.magicdev.manalgak.domain.meeting.entity.Meeting;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MeetingRepository extends JpaRepository<Meeting,Long> {
 
     Optional<Meeting> findByMeetingUuid(String meetingUuid);
+
+    Page<Meeting> findByIdIn(List<Long> meetingIds, Pageable pageable);
 
 }
