319a296ce4e4909cb9bd9e30bd871621ac91c10a
CHOSSEANG <jng00153@gmail.com>
2026-01-19 17:58:20 +0900

    feat : 중간 지점 계산 > 중간 지점 계산 테스트용 코드 추가

--- backend/src/main/java/com/magicdev/manalgak/domain/algorithm/controller/AlgorithmController.java
+++ backend/src/main/java/com/magicdev/manalgak/domain/algorithm/controller/AlgorithmController.java
@@ -1,4 +1,31 @@
 package com.magicdev.manalgak.domain.algorithm.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.magicdev.manalgak.domain.algorithm.Model.Coordinate;
+import com.magicdev.manalgak.domain.algorithm.service.MidpointCalculationService;
+
+@RestController
+@RequestMapping("/midpoint")
 public class AlgorithmController {
+
+	private MidpointCalculationService midpointCalculationService;
+
+	@PostMapping
+	public Coordinate midpointFind(@RequestBody List<Coordinate> coordinates){
+		/**
+		 * 가장 기본적인 구현 - 이것만 있어도 충분
+		 */
+		// 1단계: 기하학적 중심점 계산 (필수)
+		Coordinate midpoint = midpointCalculationService.calculateGeometricCenter(coordinates);
+
+		return midpoint;
+	}
+
 }
