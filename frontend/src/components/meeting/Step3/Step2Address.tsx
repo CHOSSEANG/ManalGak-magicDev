77a1883720676cbc3acd51a916e7780a2bb13691
madelily <madelily@naver.com>
2026-02-02 21:02:11 +0900

    오류사항 삭제 card 미사용

--- frontend/src/components/meeting/Step3/Step2Address.tsx
+++ frontend/src/components/meeting/Step3/Step2Address.tsx
@@ -9,7 +9,7 @@ import BookmarkAddressModal from "@/components/map/BookmarkAddressModal";
 import { Input } from "@/components/ui/input";
 
 // shadcn/ui
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 
 type AddressType = "origin" | "return";
