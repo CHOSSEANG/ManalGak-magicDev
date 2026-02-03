2c853f2d2bce3e33272302205422e9aa02670659
madelily <madelily@naver.com>
2026-02-03 22:26:38 +0900

    폴더명 오타 수정

--- frontend/src/app/meetings/new/step3-result/page.tsx
+++ frontend/src/app/meetings/new/step3-result/page.tsx
@@ -4,7 +4,7 @@
 import { Suspense, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import StepNavigation from "@/components/layout/StepNavigation";
-import Step3PlaceList from "@/components/meeting/Step4/Step3PlaceList";
+import Step3PlaceList from "@/components/meeting/Step3/Step3PlaceList";
 import { useRouter } from "next/navigation";
 import { useUser } from "@/context/UserContext";
 import CompletedMeetingNotice from "@/components/common/CompletedMeetingNotice";
